9a6e4d7ce42e9e6e339686a3b8de661a3a01268c
iambronnix <erickndeto202@gmail.com>
2026-06-27 15:21:48 +0300

    timing out a connection attempt with the DialTimeout()

---
+++ Network-Programming/Tcp-Connection/tcp-server/server3/dial/main_test.go
@@ -0,0 +1,37 @@
+package dial
+
+import (
+	"net"
+	"syscall"
+	"time"
+	"testing"
+)
+func DialTimeout(network, address string, timeout time.Duration)(net.Conn,error){
+	d := net.Dialer{//overide the control function and returns an error
+		Control:func(_, address string,_ syscall.RawConn) error {
+			return &net.DNSError{
+				Err:  "connection timed out",
+				Name: address,
+				Server: "127.0.0.1",
+				IsTimeout: true,
+				IsTemporary: true,
+			}
+		},
+		Timeout: timeout,
+	}
+	return d.Dial(network,address)
+}
+func TestDialTimeout(t *testing.T){
+	c, err := DialTimeout("tcp","10.0.0.1:http",5*time.Second)
+	if err  == nil{
+		c.Close()
+		t.Fatal("connection didn't timeout")
+	}
+	nErr, ok := err.(net.Error)
+	if !ok {
+		t.Fatal(err)
+	}
+	if !nErr.Timeout(){
+		t.Fatal("error isn't a timeout")
+	}
+}
