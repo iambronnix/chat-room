b90448171df0b8fcca132a7e64b1b0cc161c6dca
iambronnix <erickndeto202@gmail.com>
2026-07-06 09:16:04 +0300

    test file

---
+++ Network-Programming/Tcp-Connection/tcp-server/server1/test_main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net"
+	"testing"
+)
+func Test_main(t *testing.T){
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err!=nil{
+		t.Fatal(err)
+	}
+	defer func(){
+		_ = listener.Close()
+	}()
+	t.Logf("bound to %q",listener.Addr())
+	for {
+		conn, err := listener.Accept()
+		if err != nil{
+			log.Println(err)
+		}
+		go func(c net.Conn){
+			defer c.Close()
+		}(conn)
+	}
+}
