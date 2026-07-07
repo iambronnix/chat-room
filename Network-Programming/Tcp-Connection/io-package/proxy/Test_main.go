f57f0263d5975840297c1cbe57b53b9acc5b916e
iambronnix <erickndeto202@gmail.com>
2026-07-07 21:06:04 +0300

    proxying data btwn connections

---
+++ Network-Programming/Tcp-Connection/io-package/proxy/Test_main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+	"net"
+)
+func proxyConn(source, destination string) error{
+	connSource, err := net.Dial("TCP", source)
+	if err != nil{
+		return err
+	}
+	defer connSource.Close()
+	connDestination, err := net.Dial("tcp", destination)
+	if err != nil{
+		return err
+	}
+	defer connDestination.Close()
+	//connDestination replies to connSource
+	go func() {_,_ = io.Copy(connSource,connDestination)}()
+	//connSource messages to connDestination
+	_, err = io.Copy(connDestination,connSource)
+	return err
+}
