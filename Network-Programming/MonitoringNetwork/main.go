fbc34068e733c3c1525e8dbb561ffb933474bc2f
iambronnix <erickndeto202@gmail.com>
2026-07-27 14:08:57 +0300

    added log entries to differentiate between incoming and outgoing data

--- Network-Programming/MonitoringNetwork/main.go
+++ Network-Programming/MonitoringNetwork/main.go
@@ -7,65 +7,73 @@ import (
 	"os"
 )
 
-//monitor embeds a log.Logger  meant fr logging server's network traffic
-type Monitor struct{
+// monitor embeds a log.Logger  meant fr logging server's network traffic
+type Incomming struct{
 	*log.Logger
 }
-//write implements the io.Writer interface
-func (m *Monitor) Write(p []byte)(int, error){
-	return  len(p), m.Output(2, string(p))
+type OutGoing struct{
+	*log.Logger
+}
+// write implements the io.Writer interface
+func (I *Incomming) Write(p []byte) (int, error) {
+	return len(p), I.Output(1, string(p))
+}
+func (O *OutGoing) Write(p []byte)(int, error) {
+	return len(p), O.Output(1, string(p))
 }
 
-func ExampleMOnitor(){
-	 monitor := &Monitor{Logger: log.New(os.Stdout, "monitor:", 0)} //an instance of Monitor to os.Stdout
-		listener, err := net.Listen("tcp", "127.0.0.1:")
-		if err != nil{
-			monitor.Fatal(err)
+func ExampleMOnitor() {
+	//log instance
+	incoming := &Incomming{Logger: log.New(os.Stdout, "clientRequest:", 0)} //logs clients side request
+	outGoing := &OutGoing{Logger: log.New(os.Stdout, "serverResponse:", 0)} //logs the serverside response
+	
+	listener, err := net.Listen("tcp", "127.0.0.1:")
+	if err != nil {
+		incoming.Fatal(err)
+	}
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+
+		conn, err := listener.Accept()
+		if err != nil {
+			return
 		}
-		done := make(chan struct{})
+		defer conn.Close()
 
-		go func(){
-			defer close(done)
+		b := make([]byte, 1024)
+		r := io.TeeReader(conn, incoming) //writes clients side requests from the connection 
 
-			conn, err := listener.Accept()
-			if err != nil{
-				return
-			}
-			defer conn.Close()
+		n, err := r.Read(b)
+		if err != nil && err != io.EOF {
+			incoming.Println(err)
+			return
+		}
+		w := io.MultiWriter(conn, outGoing) //logs server response
 
-			b := make([]byte, 1024)
-			r := io.TeeReader(conn, monitor)//writes into monitor and returns an io.reader
-
-			n, err := r.Read(b)
-			if err != nil && err != io.EOF{
-				monitor.Println(err)
-				return
-			}
-			w := io.MultiWriter(conn, monitor)//duplicates its writes 
-
-			_, err = w.Write(b[:n])//echo message
-			if err != nil && err != io.EOF{
-				monitor.Println(err)
-				return
-			}
+		_, err = w.Write(b[:n]) //echo message
+		if err != nil && err != io.EOF {
+			outGoing.Println(err)
+			return
+		}
 
 	}()
 
-		conn, err := net.Dial("tcp",listener.Addr().String())//dialer to the server
-		if err != nil{
-			monitor.Fatal(err)
-		}
-		_, err = conn.Write([]byte("iamerick\n"))
-		if err != nil{
-			monitor.Fatal(err)
-		}
-		_ = conn.Close()
-		<- done		
+	conn, err := net.Dial("tcp", listener.Addr().String()) //dialer to the server
+	if err != nil {
+		outGoing.Fatal(err)
+	}
+	_, err = conn.Write([]byte("iamerick\n"))
+	if err != nil {
+		outGoing.Fatal(err)
+	}
+	_ = conn.Close()
+	<-done
 
-		
-}		
+}
 
-func main(){
+func main() {
 	ExampleMOnitor()
-	
+
 }
